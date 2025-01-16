ee0694f1d57fee3de08634bf7079215bb1e44844
orangekame3 <orangekame3.dev@gmail.com>
2025-01-16 10:23:40 +0000

    feat: add some protcols

--- backend/qcflow/subflow/qubex/flow.py
+++ backend/qcflow/subflow/qubex/flow.py
@@ -4,11 +4,22 @@ from qcflow.schema.menu import Menu
 from qcflow.subflow.qubex.task import (
     TaskManager,
     TaskResult,
+    calibrate_control_frequency_task,
+    calibrate_hpi_pulse_task,
+    calibrate_pi_pulse_task,
+    # rabi_task,
+    calibrate_readout_frequency_task,
+    check_hpi_pulse_task,
     check_noise_task,
+    check_pi_pulse_task,
+    check_rabi_task,
     check_status_task,
+    # chevron_pattern_task,
     configure_task,
+    effective_control_frequency_task,
     linkup_task,
-    rabi_task,
+    t1_task,
+    t2_task,
 )
 from qubex.experiment import Experiment
 
@@ -30,15 +41,44 @@ def qubex_flow(
     logger.info("Starting qubex_flow")
     exp = Experiment(
         chip_id="64Q",
-        qubits=[4, 5, 6, 7],
+        qubits=[5, 7],
         config_dir="/home/shared/config",
     )
+    exp.note.clear()
     task_manager = TaskManager()
     dummy_result = TaskResult(name="dummy", status="pending", message="")
     check_status_result = check_status_task.submit(exp, task_manager, dummy_result).result()
     linkup_result = linkup_task.submit(exp, task_manager, check_status_result).result()
     configure_result = configure_task.submit(exp, task_manager, linkup_result).result()
     check_noise_result = check_noise_task.submit(exp, task_manager, configure_result).result()
-    rabi_task.submit(exp, task_manager, check_noise_result).result()
+    # rabi_result = rabi_task.submit(exp, task_manager, check_noise_result).result()
+    # chevron_pattern_result = chevron_pattern_task.submit(exp, task_manager, rabi_result).result()
+    calibrate_control_frequency_result = calibrate_control_frequency_task.submit(
+        exp, task_manager, check_noise_result
+    ).result()
+    calibrate_readout_frequency_result = calibrate_readout_frequency_task.submit(
+        exp, task_manager, calibrate_control_frequency_result
+    ).result()
+    check_rabi_result = check_rabi_task.submit(
+        exp, task_manager, calibrate_readout_frequency_result
+    ).result()
+    calibrate_hpi_pulse_result = calibrate_hpi_pulse_task.submit(
+        exp, task_manager, check_rabi_result
+    ).result()
+    check_hpi_pulse_result = check_hpi_pulse_task.submit(
+        exp, task_manager, calibrate_hpi_pulse_result
+    ).result()
+    calibrate_pi_pulse_result = calibrate_pi_pulse_task.submit(
+        exp, task_manager, check_hpi_pulse_result
+    ).result()
+    check_pi_pulse_result = check_pi_pulse_task.submit(
+        exp, task_manager, calibrate_pi_pulse_result
+    ).result()
+    t1_result = t1_task.submit(exp, task_manager, check_pi_pulse_result).result()
+    t2_result = t2_task.submit(exp, task_manager, t1_result).result()
+    effective_control_frequency_result = effective_control_frequency_task.submit(
+        exp, task_manager, t2_result
+    ).result()
+
     logger.info(f"Final State: {task_manager.model_dump()}")
     return successMap
--- backend/qcflow/subflow/qubex/task.py
+++ backend/qcflow/subflow/qubex/task.py
@@ -1,8 +1,7 @@
+import numpy as np
 from prefect import get_run_logger, task
 from pydantic import BaseModel
 from qubex.experiment import Experiment
-
-logger = get_run_logger()
 
 
 class TaskResult(BaseModel):
@@ -16,7 +15,6 @@ class TaskResult(BaseModel):
         """
         self.status = "failed"
         self.message = message
-        logger.info(f"Task {self.name} failed with message: {message}")
 
     def update_status_to_success(self, message: str):
         """
@@ -24,16 +22,13 @@ class TaskResult(BaseModel):
         """
         self.status = "success"
         self.message = message
-        logger.info(f"Task {self.name} is successful with message: {message}")
 
     def diagnose(self):
         """
         diagnose the task result and raise an error if the task failed.
         """
         if self.status == "failed":
-            logger.error(f"Task {self.name} failed with message: {self.message}")
             raise RuntimeError(f"Task {self.name} failed with message: {self.message}")
-        logger.info(f"Task {self.name} is successful with message: {self.message}")
 
 
 class TaskManager(BaseModel):
@@ -42,16 +37,39 @@ class TaskManager(BaseModel):
     configure: TaskResult = TaskResult(name="configure", status="pending", message="")
     check_noise: TaskResult = TaskResult(name="check-noise", status="pending", message="")
     rabi: TaskResult = TaskResult(name="rabi", status="pending", message="")
+    chevron_pattern: TaskResult = TaskResult(name="chevron-pattern", status="pending", message="")
+    calibrate_control_frequency: TaskResult = TaskResult(
+        name="calibrate-control-frequency", status="pending", message=""
+    )
+    calibrate_readout_frequency: TaskResult = TaskResult(
+        name="calibrate-readout-frequency", status="pending", message=""
+    )
+    check_rabi: TaskResult = TaskResult(name="check-rabi", status="pending", message="")
+    calibrate_hpi_pulse: TaskResult = TaskResult(
+        name="calibrate-hpi-pulse", status="pending", message=""
+    )
+    check_hpi_pulse: TaskResult = TaskResult(name="check-hpi-pulse", status="pending", message="")
+    calibrate_pi_pulse: TaskResult = TaskResult(
+        name="calibrate-pi-pulse", status="pending", message=""
+    )
+    check_pi_pulse: TaskResult = TaskResult(name="check-pi-pulse", status="pending", message="")
+    t1: TaskResult = TaskResult(name="t1", status="pending", message="")
+    t2: TaskResult = TaskResult(name="t2", status="pending", message="")
+    effective_control_frequency: TaskResult = TaskResult(
+        name="effective-control-frequency", status="pending", message=""
+    )
 
 
 @task(name="check-status")
 def check_status_task(
     exp: Experiment, task_manager: TaskManager, prev_result: TaskResult
 ) -> TaskResult:
+    logger = get_run_logger()
     prev_result.diagnose()
     try:
         logger.info("Starting check status")
         exp.check_status()
+        exp.save_defaults()
     except Exception as e:
         task_manager.check_status.update_status_to_failed(f"Failed to check status: {e}")
         raise RuntimeError(f"Failed to check status: {e}")
@@ -61,10 +79,12 @@ def check_status_task(
 
 @task(name="linkup")
 def linkup_task(exp: Experiment, task_manager: TaskManager, prev_result: TaskResult) -> TaskResult:
+    logger = get_run_logger()
     prev_result.diagnose()
     try:
         logger.info("Starting linkup")
         exp.linkup()
+        exp.save_defaults()
     except Exception as e:
         task_manager.linkup.update_status_to_failed(f"Failed to linkup: {e}")
         raise RuntimeError(f"Failed to linkup: {e}")
@@ -76,10 +96,12 @@ def linkup_task(exp: Experiment, task_manager: TaskManager, prev_result: TaskRes
 def configure_task(
     exp: Experiment, task_manager: TaskManager, prev_result: TaskResult
 ) -> TaskResult:
+    logger = get_run_logger()
     prev_result.diagnose()
     try:
         logger.info("Starting configure")
         exp.configure(confirm=False)
+        exp.save_defaults()
     except Exception as e:
         task_manager.configure.update_status_to_failed(f"Failed to configure: {e}")
         raise RuntimeError(f"Failed to configure: {e}")
@@ -91,10 +113,12 @@ def configure_task(
 def check_noise_task(
     exp: Experiment, task_manager: TaskManager, prev_result: TaskResult
 ) -> TaskResult:
+    logger = get_run_logger()
     prev_result.diagnose()
     try:
         logger.info("Starting check noise")
         exp.check_noise()
+        exp.save_defaults()
     except Exception as e:
         task_manager.check_noise.update_status_to_failed(f"Failed to check noise: {e}")
         raise RuntimeError(f"Failed to check noise: {e}")
@@ -104,6 +128,7 @@ def check_noise_task(
 
 @task(name="rabi")
 def rabi_task(exp: Experiment, task_manager: TaskManager, prev_result: TaskResult) -> TaskResult:
+    logger = get_run_logger()
     prev_result.diagnose()
     default_rabi_amplitudes = {label: 0.01 for label in exp.qubit_labels}
     try:
@@ -115,8 +140,267 @@ def rabi_task(exp: Experiment, task_manager: TaskManager, prev_result: TaskResul
             shots=300,
             interval=50_000,
         )
+        exp.save_defaults()
     except Exception as e:
         task_manager.rabi.update_status_to_failed(f"Failed to run Rabi experiment: {e}")
         raise RuntimeError(f"Failed to run Rabi experiment: {e}")
     task_manager.rabi.update_status_to_success("Rabi experiment is successful.")
     return task_manager.rabi
+
+
+@task(name="chevron-pattern")
+def chevron_pattern_task(
+    exp: Experiment, task_manager: TaskManager, prev_result: TaskResult
+) -> TaskResult:
+    logger = get_run_logger()
+    prev_result.diagnose()
+    try:
+        logger.info("Starting Chevron pattern experiment")
+        exp.chevron_pattern(
+            exp.qubit_labels,
+            detuning_range=np.linspace(-0.05, 0.05, 51),
+            time_range=np.arange(0, 201, 4),
+        )
+        exp.save_defaults()
+    except Exception as e:
+        task_manager.chevron_pattern.update_status_to_failed(
+            f"Failed to run Chevron pattern experiment: {e}"
+        )
+        raise RuntimeError(f"Failed to run Chevron pattern experiment: {e}")
+    task_manager.chevron_pattern.update_status_to_success(
+        "Chevron pattern experiment is successful."
+    )
+    return task_manager.chevron_pattern
+
+
+@task(name="calibrate-control-frequency")
+def calibrate_control_frequency_task(
+    exp: Experiment, task_manager: TaskManager, prev_result: TaskResult
+) -> TaskResult:
+    logger = get_run_logger()
+    prev_result.diagnose()
+    try:
+        logger.info("Starting control frequency calibration")
+        exp.calibrate_control_frequency(
+            exp.qubit_labels,
+            detuning_range=np.linspace(-0.01, 0.01, 21),
+            time_range=range(0, 101, 4),
+        )
+        exp.save_defaults()
+    except Exception as e:
+        task_manager.calibrate_control_frequency.update_status_to_failed(
+            f"Failed to calibrate control frequency: {e}"
+        )
+        raise RuntimeError(f"Failed to calibrate control frequency: {e}")
+    task_manager.calibrate_control_frequency.update_status_to_success(
+        "Control frequency calibration is successful."
+    )
+    return task_manager.calibrate_control_frequency
+
+
+@task(name="calibrate-readout-frequency")
+def calibrate_readout_frequency_task(
+    exp: Experiment, task_manager: TaskManager, prev_result: TaskResult
+) -> TaskResult:
+    logger = get_run_logger()
+    prev_result.diagnose()
+    try:
+        logger.info("Starting readout frequency calibration")
+        exp.calibrate_readout_frequency(
+            exp.qubit_labels,
+            detuning_range=np.linspace(-0.01, 0.01, 21),
+            time_range=range(0, 101, 4),
+        )
+        exp.save_defaults()
+    except Exception as e:
+        task_manager.calibrate_readout_frequency.update_status_to_failed(
+            f"Failed to calibrate readout frequency: {e}"
+        )
+        raise RuntimeError(f"Failed to calibrate readout frequency: {e}")
+    task_manager.calibrate_readout_frequency.update_status_to_success(
+        "Readout frequency calibration is successful."
+    )
+    return task_manager.calibrate_readout_frequency
+
+
+@task(name="check-rabi")
+def check_rabi_task(
+    exp: Experiment, task_manager: TaskManager, prev_result: TaskResult
+) -> TaskResult:
+    logger = get_run_logger()
+    prev_result.diagnose()
+    try:
+        logger.info("Starting check rabi")
+        exp.check_rabi()
+        exp.save_defaults()
+    except Exception as e:
+        task_manager.check_rabi.update_status_to_failed(f"Failed to check rabi: {e}")
+        raise RuntimeError(f"Failed to check rabi: {e}")
+    task_manager.check_rabi.update_status_to_success("Check rabi is successful.")
+    return task_manager.check_rabi
+
+
+@task(name="calibrate-hpi-pulse")
+def calibrate_hpi_pulse_task(
+    exp: Experiment, task_manager: TaskManager, prev_result: TaskResult
+) -> TaskResult:
+    logger = get_run_logger()
+    prev_result.diagnose()
+    try:
+        logger.info("Starting calibrate HPI pulse")
+        exp.calibrate_hpi_pulse(
+            exp.qubit_labels,
+            n_rotations=1,
+        )
+        exp.save_defaults()
+    except Exception as e:
+        task_manager.calibrate_hpi_pulse.update_status_to_failed(
+            f"Failed to calibrate HPI pulse: {e}"
+        )
+        raise RuntimeError(f"Failed to calibrate HPI pulse: {e}")
+    task_manager.calibrate_hpi_pulse.update_status_to_success("Calibrate HPI pulse is successful.")
+    return task_manager.calibrate_hpi_pulse
+
+
+@task(name="check-hpi-pulse")
+def check_hpi_pulse_task(
+    exp: Experiment, task_manager: TaskManager, prev_result: TaskResult
+) -> TaskResult:
+    logger = get_run_logger()
+    prev_result.diagnose()
+    try:
+        logger.info("Starting check HPI pulse")
+        exp.repeat_sequence(
+            exp.hpi_pulse,
+            repetitions=20,
+        )
+        exp.save_defaults()
+    except Exception as e:
+        task_manager.check_hpi_pulse.update_status_to_failed(f"Failed to check HPI pulse: {e}")
+        raise RuntimeError(f"Failed to check HPI pulse: {e}")
+    task_manager.check_hpi_pulse.update_status_to_success("Check HPI pulse is successful.")
+    return task_manager.check_hpi_pulse
+
+
+@task(name="calibrate-pi-pulse")
+def calibrate_pi_pulse_task(
+    exp: Experiment, task_manager: TaskManager, prev_result: TaskResult
+) -> TaskResult:
+    logger = get_run_logger()
+    prev_result.diagnose()
+    try:
+        logger.info("Starting calibrate pi pulse")
+        exp.calibrate_pi_pulse(
+            exp.qubit_labels,
+            n_rotations=1,
+        )
+        exp.save_defaults()
+    except Exception as e:
+        task_manager.calibrate_pi_pulse.update_status_to_failed(
+            f"Failed to calibrate pi pulse: {e}"
+        )
+        raise RuntimeError(f"Failed to calibrate pi pulse: {e}")
+    task_manager.calibrate_pi_pulse.update_status_to_success("Calibrate pi pulse is successful.")
+    return task_manager.calibrate_pi_pulse
+
+
+@task(name="check-pi-pulse")
+def check_pi_pulse_task(
+    exp: Experiment, task_manager: TaskManager, prev_result: TaskResult
+) -> TaskResult:
+    logger = get_run_logger()
+    prev_result.diagnose()
+    try:
+        logger.info("Starting check pi pulse")
+        exp.repeat_sequence(
+            exp.pi_pulse,
+            repetitions=20,
+        )
+        exp.save_defaults()
+    except Exception as e:
+        task_manager.check_pi_pulse.update_status_to_failed(f"Failed to check pi pulse: {e}")
+        raise RuntimeError(f"Failed to check pi pulse: {e}")
+    task_manager.check_pi_pulse.update_status_to_success("Check pi pulse is successful.")
+    return task_manager.check_pi_pulse
+
+
+@task(name="t1")
+def t1_task(exp: Experiment, task_manager: TaskManager, prev_result: TaskResult) -> TaskResult:
+    logger = get_run_logger()
+    prev_result.diagnose()
+    try:
+        logger.info("Starting T1 experiment")
+        t1_result = exp.t1_experiment(
+            time_range=np.logspace(
+                np.log10(100),
+                np.log10(500 * 1000),
+                51,
+            ),
+            save_image=True,
+        )
+        t1_values = {}
+        for qubit in exp.qubit_labels:
+            t1_values[qubit] = t1_result.data[qubit].t1 if qubit in t1_result.data else None
+        exp.note.put("t1", t1_values)
+        exp.note.save()
+        print(t1_values)
+    except Exception as e:
+        task_manager.t1.update_status_to_failed(f"Failed to run T1 experiment: {e}")
+        raise RuntimeError(f"Failed to run T1 experiment: {e}")
+    task_manager.t1.update_status_to_success("T1 experiment is successful.")
+    return task_manager.t1
+
+
+@task(name="t2")
+def t2_task(exp: Experiment, task_manager: TaskManager, prev_result: TaskResult) -> TaskResult:
+    logger = get_run_logger()
+    prev_result.diagnose()
+    try:
+        logger.info("Starting T2 experiment")
+        t2_result = exp.t2_experiment(
+            exp.qubit_labels,
+            time_range=np.logspace(
+                np.log10(300),
+                np.log10(100 * 1000),
+                51,
+            ),
+            save_image=True,
+        )
+        t2_values = {}
+        for qubit in exp.qubit_labels:
+            t2_values[qubit] = t2_result.data[qubit].t2 if qubit in t2_result.data else None
+        exp.note.put("t2", t2_values)
+        exp.note.save()
+        print(t2_values)
+    except Exception as e:
+        task_manager.t2.update_status_to_failed(f"Failed to run T2 experiment: {e}")
+        raise RuntimeError(f"Failed to run T2 experiment: {e}")
+    task_manager.t2.update_status_to_success("T2 experiment is successful.")
+    return task_manager.t2
+
+
+@task(name="effective-control-frequency")
+def effective_control_frequency_task(
+    exp: Experiment, task_manager: TaskManager, prev_result: TaskResult
+) -> TaskResult:
+    logger = get_run_logger()
+    prev_result.diagnose()
+    try:
+        logger.info("Starting effective control frequency calibration")
+        effective_control_frequency_result = exp.obtain_effective_control_frequency(
+            exp.qubit_labels,
+            time_range=np.arange(0, 20001, 100),
+            detuning=0.001,
+        )
+        effective_freq = effective_control_frequency_result["effective_freq"]
+        exp.note.put("effective_freq", effective_freq)
+        exp.note.save()
+    except Exception as e:
+        task_manager.effective_control_frequency.update_status_to_failed(
+            f"Failed to calibrate effective control frequency: {e}"
+        )
+        raise RuntimeError(f"Failed to calibrate effective control frequency: {e}")
+    task_manager.effective_control_frequency.update_status_to_success(
+        "Effective control frequency calibration is successful."
+    )
+    return task_manager.effective_control_frequency
