72e1757c045146eceaaf24a0453004d9469e11b3
orangekame3 <orangekame3.dev@gmail.com>
2025-02-25 07:50:24 +0000

    move execution page

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,23 +1,34 @@
-import './globals.css';
-import Providers from './providers';
+import "./globals.css";
+import Providers from "./providers";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 
 export const metadata = {
-  title: 'QDash',
-  description: 'Quantum Dashboard',
-}
+  title: "QDash",
+  description: "Quantum Dashboard",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>
+      <body suppressHydrationWarning>
         <Providers>
-          {children}
+          <div className="drawer lg:drawer-open">
+            <input id="drawer" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content flex flex-col">
+              <Navbar />
+              <main className="flex-1 overflow-y-auto bg-base-100">
+                {children}
+              </main>
+            </div>
+            <Sidebar />
+          </div>
         </Providers>
       </body>
     </html>
-  )
+  );
 }
