95db11a62af1934b95bdcbf44b862b15edd81627
Takafumi Miyanaga <orangekame3.dev@gmail.com>
2025-06-16 10:53:43 +0900

    feat: Add timeout of 25 minutes to multiple coupling tasks (#205)

--- src/qdash/workflow/tasks/two_qubit/check_bell_state.py
+++ src/qdash/workflow/tasks/two_qubit/check_bell_state.py
@@ -16,6 +16,7 @@ class CheckBellState(BaseTask):
 
     name: str = "CheckBellState"
     task_type: str = "coupling"
+    timeout: int = 60 * 25  # 25 minutes
     input_parameters: ClassVar[dict[str, InputParameterModel]] = {}
     output_parameters: ClassVar[dict[str, OutputParameterModel]] = {}
 
